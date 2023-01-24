5966286fbe3975a3d023c081aafb887997cdaa51
Viktor <viktor_9@list.ru>
2023-01-24 22:08:55 +0300

    commit-three

--- Homework-Union-File.py
+++ Homework-Union-File.py
@@ -1,71 +1,23 @@
-name_file = '1.txt'
-name_file2 = '2.txt'
-name_file3 = '3.txt'
-with open('1.txt', 'r', encoding='utf-8') as file_one:
-    f1 = file_one.readlines()
-with open('2.txt', 'r', encoding='utf-8') as file_two:
-    f2 = file_two.readlines()
-with open('3.txt', 'r', encoding='utf-8') as file_three:
-    f3 = file_three.readlines()
-with open('4.txt', 'w', encoding='utf-8') as file_four:
-    if len(f1) < len(f2) and len(f3):
-        file_four.write(name_file + '\n')
-        file_four.write(str(len(f1)) + '\n')
-        file_four.writelines(f1)
-        file_four.write('\n')
-        if len(f2) < len(f3):
-            file_four.write(name_file2 + '\n')
-            file_four.write(str(len(f2)) + '\n')
-            file_four.writelines(f2)
-            file_four.write('\n')
-            file_four.write(name_file3 + '\n')
-            file_four.write(str(len(f3)) + '\n')
-        else:
-            file_four.write(name_file3 + '\n')
-            file_four.write(str(len(f3)) + '\n')
-            file_four.writelines(f3)
-            file_four.write(name_file2 + '\n')
-            file_four.write(str(len(f2)) + '\n')
-            file_four.writelines(f2)
-    if len(f2) < len(f1) and len(f3):
-        file_four.write(name_file2 + '\n')
-        file_four.write(str(len(f2)) + '\n')
-        file_four.writelines(f2)
-        file_four.write('\n')
-        if len(f1) < len(f3):
-            file_four.write(name_file + '\n')
-            file_four.write(str(len(f1)) + '\n')
-            file_four.writelines(f1)
-            file_four.write('\n')
-            file_four.write(name_file3 + '\n')
-            file_four.write(str(len(f3)) + '\n')
-            file_four.writelines(f3)
-        else:
-            file_four.write(name_file3 + '\n')
-            file_four.write(str(len(f3)) + '\n')
-            file_four.writelines(f3)
-            file_four.write('\n')
-            file_four.write(name_file + '\n')
-            file_four.write(str(len(f1)) + '\n')
-            file_four.writelines(f1)
-    if len(f3) < len(f1) and len(f2):
-        file_four.write(name_file3 + '\n')
-        file_four.write(str(len(f3)) + '\n')
-        file_four.writelines(f3)
-        file_four.write('\n')
-        if len(f1) < len(f2):
-            file_four.write(name_file + '\n')
-            file_four.write(str(len(f1)) + '\n')
-            file_four.writelines(f1)
-            file_four.write('\n')
-            file_four.write(name_file2 + '\n')
-            file_four.write(str(len(f2)) + '\n')
-            file_four.writelines(f2)
-        else:
-            file_four.write(name_file2 + '\n')
-            file_four.write(str(len(f2)) + '\n')
-            file_four.writelines(f2)
-            file_four.write('\n')
-            file_four.write(name_file + '\n')
-            file_four.write(str(len(f1)) + '\n')
-            file_four.writelines(f1)
+with open('1.txt', 'r', encoding='utf-8') as file1:
+    f1 = file1.readlines()
+with open('2.txt', 'r', encoding='utf-8') as file2:
+    f2 = file2.readlines()
+with open('3.txt', 'r', encoding='utf-8') as file3:
+    f3 = file3.readlines()
+dict_ = {'1.txt': len(f1), '2.txt': len(f2), '3.txt': len(f3)}
+dict_values = sorted(dict_.values())
+dict_files = {}
+for i in dict_values:
+    for w in dict_.keys():
+        if dict_[w] == i:
+            dict_files[w] = dict_[w]
+            break
+with open('4.txt', 'w', encoding='utf-8') as file4:
+    for name_file in dict_files:
+        with open(name_file, 'r', encoding='utf-8') as file:
+            f = file.readlines()
+            file4.write(name_file + '\n')
+            file4.write(str(len(f)) + '\n')
+            file4.writelines(f)
+            file4.write('\n')
+print(dict_files)
